e42a4ae83e5944bbd48d1ede371ad918ada52590
HackerTheBestYTB <56880221+HackerTheBestYTB@users.noreply.github.com>
2022-08-21 23:14:53 +0700

    Update DOORS.lua

--- DOORS.lua
+++ DOORS.lua
@@ -160,6 +160,28 @@ general:Button("Comming Soon",function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerTheBestYTB/ScriptRoblox/test/loaadeer.lua"))()
 end)
 
+r50:Button("Monster",function()
+for i,v in pairs(game.Workspace:GetDescendants()) do -- grabs everything from workspace
+	if v.Parent.Name == 'FigureRagdoll' then -- checks if it has a handle and if its a touchtransmitter
+		local BillboardGui = Instance.new('BillboardGui') -- Makes Billboardgui
+		local TextLabel = Instance.new('TextLabel') -- makes text label
+
+		BillboardGui.Parent = v.Parent -- what the billboardgui goes into
+		BillboardGui.AlwaysOnTop = true -- if its on top or not
+		BillboardGui.Size = UDim2.new(0, 50, 0, 50) -- size of it
+		BillboardGui.StudsOffset = Vector3.new(0,2,0)
+
+		TextLabel.Parent = BillboardGui -- putting textlabel into billboardgui
+		TextLabel.BackgroundColor3 = Color3.new(1,1,1) -- color
+		TextLabel.BackgroundTransparency = 1 -- transparency
+		TextLabel.Size = UDim2.new(1, 0, 1, 0) -- size
+		TextLabel.Text = "Monster" -- what the label says
+		TextLabel.TextColor3 = Color3.new(1, 0, 0) -- color
+		TextLabel.TextScaled = true -- if the text is scaled or not
+		end
+	end
+end)
+
 r50:Button("Book", function()
 	for i,v in pairs(game.Workspace.CurrentRooms:GetDescendants()) do -- grabs everything from workspace
 		if v.Parent.Name == 'LiveHintBook' then -- checks if it has a handle and if its a touchtransmitter
