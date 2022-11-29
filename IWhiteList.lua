85ac8fb310fc399f802164f0a5fef0d53be3de0f
HackerTheBestYTB <56880221+HackerTheBestYTB@users.noreply.github.com>
2022-11-29 20:25:42 +0700

    Update IWhiteList.lua

--- IWhiteList.lua
+++ IWhiteList.lua
@@ -5,9 +5,14 @@ wait(2)
 
 if ID == "1" then -- BEF49A83-21F6-4BB5-967E-3A106BB817AC
 	game.StarterGui:SetCore("SendNotification", {Title="TVNHUB"; Text="Thanh Cong!"; Duration=1;})
-elseif ID == "B68963E1-E753-4271-9317-6961DF4E27C1" then
+elseif ID == "B68963E1-E753-4271-9317-6961DF4E27C1" then --Triet2804
 	game.StarterGui:SetCore("SendNotification", {Title="TVNHUB"; Text="Thanh Cong!"; Duration=1;})
-	allow = true
+elseif ID == "b73f314e-5282-42df-8403-d4bd037971e7" then --PTA Mobile
+	game.StarterGui:SetCore("SendNotification", {Title="TVNHUB"; Text="Thanh Cong!"; Duration=1;})
+elseif ID == "4" then
+	game.StarterGui:SetCore("SendNotification", {Title="TVNHUB"; Text="Thanh Cong!"; Duration=1;})
+elseif ID == "5" then
+	game.StarterGui:SetCore("SendNotification", {Title="TVNHUB"; Text="Thanh Cong!"; Duration=1;})
 else	
 	game.StarterGui:SetCore("SendNotification", {Title="TVNHUB"; Text="Ban ko co trong whitelist!"; Duration=1;})
 	game.StarterGui:SetCore("SendNotification", {Title="TVNHUB"; Text="Da copy id, vui long gui admin!"; Duration=1;})
