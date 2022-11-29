813f221e1b8416a02352d85612d91cd8dc0e8d2d
HackerTheBestYTB <56880221+HackerTheBestYTB@users.noreply.github.com>
2022-11-29 17:48:56 +0700

    Create Invisible-Main.lua

---
+++ Invisible-Main.lua
@@ -0,0 +1,11 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerTheBestYTB/ScriptRoblox/test/RemoteSpy.lua"))()
+function loadscript()
+print("Success")
+end
+
+if allow == true then
+loadscript()
+else
+	game.Players.LocalPlayer:Kick("🤨")
+end
+--🤨
