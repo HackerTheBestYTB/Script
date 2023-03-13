40e7a42830812fe33c3c1e8acfa73e47fab3b3c6
HackerTheBestYTB <56880221+HackerTheBestYTB@users.noreply.github.com>
2023-03-13 10:55:41 +0700

    Update AutoFruit.lua

--- AutoFruit.lua
+++ AutoFruit.lua
@@ -14,20 +14,4 @@ _G.ServerHopCommandChat = "mh" --ghi trên chat là mh để đổi server/ dàn
 _G.S = "1" --Mode 1 và 2 của Server Hop/ tránh đụng với hacker khác:)
 _G.AutoTeam = "2" --1 là Pirates/ 2 là Marines
 ---------------------------------------------------------------------------------
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/HackerTheBestYTB/ScriptRoblox/test/AF-Fix.lua")))()_G.Key = "toingaychoibloxfruit"
---------------------------------------------------------------------------------
-_G.Mode = "1" --1 là mặc định/2 là chỉnh thủ công như bên dưới
-_G.TweenToFruit = true --false để dừng tween di chuyển thoải mái
-_G.TweenToAllFruit = false --true nhặt tất cả fruit/false chỉ nhặt fruit spawn
-_G.AutoStore = true --AutoStore chỉ store những trái từ Door trở lên(lười thêm)
-_G.BypassKickWhenTween = true --Không kick khi tween
-_G.AutoServerHop = true --Tự Động Đổi Server/ có lỗi nhưng chưa gặp:)
-_G.AutoServerHopWhenStuck = true --Tự động chuyển server khi kẹt
-_G.AutoServerHopWhenStuckTime = 90 --90 ở đây là 90 giây
-_G.ServerHopKey = "f" --Đổi Server Thủ Công/ Ko ghi bằng CapLock/Ko cầm item gì
-_G.ServerHopCommand = false --tắt/bật command chat / dành mobile
-_G.ServerHopCommandChat = "mh" --ghi trên chat là mh để đổi server/ dành mobile
-_G.S = "1" --Mode 1 và 2 của Server Hop/ tránh đụng với hacker khác:)
-_G.AutoTeam = "2" --1 là Pirates/ 2 là Marines
----------------------------------------------------------------------------------
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/HackerTheBestYTB/ScriptRoblox/test/AF-Fix.lua")))()
